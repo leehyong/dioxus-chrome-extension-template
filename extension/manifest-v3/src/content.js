2f5d0a5f4e02b99b08874de4efcd93007feff50d
lihuayong <lihy47@chinatelecom.cn>
2024-05-25 18:07:49 +0800

    update cargo.toml

--- extension/manifest-v3/src/content.js
+++ extension/manifest-v3/src/content.js
@@ -1,13 +1,14 @@
+// 创建挂载点存放wasm
 const better_spider_id = 'better-spider-box';
 const div = document.createElement('div')
 div.setAttribute('id', better_spider_id);
 document.body.appendChild(div);
-
+// 创建style，链接better-spider样式
 const link = document.createElement("link");
 link.href = chrome.runtime.getURL("src/better-spider.css");
 link.type = "text/css";
 link.rel = "stylesheet";
 document.getElementsByTagName("head")[0].appendChild(link);
-
+document.oncontextmenu = () => false; //右键菜单屏蔽
 import { run } from './wasm/better_spider';
 run();
