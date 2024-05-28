42f6607b4fd3a04738aa4a2c6fd93070cd84e8d4
lihuayong <lihy47@chinatelecom.cn>
2024-05-28 20:33:12 +0800

    changed color

--- extension/src/lib.rs
+++ extension/src/lib.rs
@@ -3,12 +3,13 @@
 #![allow(dead_code)]
 use std::borrow::Borrow;
 
+use dioxus::html::mo;
 use dioxus::prelude::*;
 use dioxus::signals::Readable;
 use dioxus::web::Config;
 use futures::StreamExt;
 use gloo::events::EventListener;
-use tracing::{info, Level};
+use tracing::{debug, info, Level};
 use wasm_bindgen::prelude::*;
 
 mod global;
@@ -42,62 +43,7 @@ pub fn launch_run() {
 
 fn App() -> Element {
     let cor = use_coroutine(move |rx| async move {
-        let window = web_sys::window().expect("should have a window in this context");
-        let doc_ = window.document().expect("window should have a document");
-        let doc_clone = doc_.clone();
-        let mut current_element_xpath = "".to_string();
-        // let mut current_element:Element;
-        info!("start element coroutine");
-        let doc_listener = EventListener::new(&doc_, "mousemove", move |event| {
-            info!("start element coroutine in mousemove");
-            let event = event.dyn_ref::<web_sys::MouseEvent>().unwrap_throw();
-            let spider_box = doc_clone
-                .get_element_by_id(SPIDER_BOX_ID)
-                .expect(&format!("should have a '{}' of div ", SPIDER_BOX_ID));
-            let spider_box_html = spider_box.dyn_ref::<HtmlElement>().unwrap_throw();
-            let x = event.client_x();
-            let y = event.client_y();
-            let x_box_offset_left = spider_box_html.offset_left();
-            let y_box_offset_top = spider_box_html.offset_top();
-            let box_offset_width = spider_box_html.offset_width();
-            let box_offset_height = spider_box_html.offset_height();
-            let x_max = x_box_offset_left + box_offset_width;
-            let y_max = y_box_offset_top + box_offset_height;
-            // check if the mouse moves into the spider boox
-            if x >= x_box_offset_left && x <= x_max && y >= y_box_offset_top && y <= y_max {
-                info!("mouse in the spider box");
-                return;
-            }
-            if let Some(mouse_element) = doc_clone.element_from_point(x as f32, y as f32) {
-                let mouse_element_xpath = element_xpath(mouse_element);
-                info!("mouse_element_xpath:{mouse_element_xpath}");
-                if mouse_element_xpath == *current_element_xpath.as_str() {
-                    info!("mouse element is the same with the current element!");
-                    return;
-                }
-                current_element_xpath = mouse_element_xpath;
-                // // remove related css class
-                // if let Some(old_ele) = &*old_element.read() {
-                //     old_ele
-                //         .dyn_ref::<HtmlElement>()
-                //         .unwrap()
-                //         .class_list()
-                //         .remove_1(HIGHLIGHT_CLASS)
-                //         .unwrap_or_default();
-                // }
-                // mouse_element
-                //     .dyn_ref::<HtmlElement>()
-                //     .unwrap()
-                //     .class_list()
-                //     .add_1(HIGHLIGHT_CLASS)
-                //     .unwrap_or_default();
-                // old_element.set(cur_ele);
-                // current_element.set(Some(mouse_element.clone()));
-                // info!("changed current element!{mouse_element:?}")
-            }
-        });
-        let doc_box = Box::new(doc_listener);
-        Box::leak(doc_box);
+        init_document_mousemove_event();
     });
     cor.send(1);
     let mut count = use_signal(|| 0);
@@ -110,4 +56,61 @@ fn App() -> Element {
             button { onclick: move |_| count -= 1, "Down low!" }
         }
     }
+}
+
+fn init_document_mousemove_event() {
+    let window = web_sys::window().expect("should have a window in this context");
+    let doc_ = window.document().expect("window should have a document");
+    let doc_clone = doc_.clone();
+    // let mut current_element_xpath = "".to_string();
+    let mut cur_element = None;
+    let mut old_element = None;
+    debug!("start element coroutine");
+    let doc_listener = EventListener::new(&doc_, "mousemove", move |event| {
+        debug!("start element coroutine in mousemove");
+        let event = event.dyn_ref::<web_sys::MouseEvent>().unwrap_throw();
+        let spider_box = doc_clone
+            .get_element_by_id(SPIDER_BOX_ID)
+            .expect(&format!("should have a '{}' of div ", SPIDER_BOX_ID));
+        let spider_box_html = spider_box.dyn_ref::<HtmlElement>().unwrap_throw();
+        let x = event.client_x();
+        let y = event.client_y();
+        let x_box_offset_left = spider_box_html.offset_left();
+        let y_box_offset_top = spider_box_html.offset_top();
+        let box_offset_width = spider_box_html.offset_width();
+        let box_offset_height = spider_box_html.offset_height();
+        let x_max = x_box_offset_left + box_offset_width;
+        let y_max = y_box_offset_top + box_offset_height;
+        // check if the mouse moves into the spider boox
+        if x >= x_box_offset_left && x <= x_max && y >= y_box_offset_top && y <= y_max {
+            debug!("mouse in the spider box");
+            return;
+        }
+        if let Some(mouse_element) = doc_clone.element_from_point(x as f32, y as f32) {
+            if let Some(cur) = &cur_element {
+                if mouse_element == *cur {
+                    debug!("mouse element is the same with the current element!");
+                    return;
+                }
+                old_element = Some(cur.clone());
+            }
+            if let Some(old) = &old_element {
+                old.dyn_ref::<HtmlElement>()
+                    .unwrap()
+                    .class_list()
+                    .remove_1(HIGHLIGHT_CLASS)
+                    .unwrap_or_default();
+            }
+            mouse_element
+                .dyn_ref::<HtmlElement>()
+                .unwrap()
+                .class_list()
+                .add_1(HIGHLIGHT_CLASS)
+                .unwrap_or_default();
+            cur_element = Some(mouse_element.clone());
+            debug!("changed current element!{mouse_element:?}")
+        }
+    });
+    let doc_box = Box::new(doc_listener);
+    Box::leak(doc_box);
 }
